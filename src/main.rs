0784bcb95481c71e5ef1bcd86ab62ec83ef720bc
Anukul Pandey <anukul.030601@gmail.com>
2025-07-22 13:57:28 +0530

    feat: logs

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,84 @@
-extern crate dotenv;
-mod html_templates;
-use html_templates::{generate_index_page, generate_random_page};
-use opentelemetry::global::shutdown_tracer_provider;
-use opentelemetry::sdk::Resource;
-use opentelemetry::trace::TraceError;
-use opentelemetry::{
-    global, sdk::trace as sdktrace,
-    trace::{TraceContextExt, Tracer},
-    Context, Key, KeyValue,
-};
-use opentelemetry_otlp::WithExportConfig;
-use rand::Rng;
-use std::error::Error;
-use tonic::metadata::{MetadataMap, MetadataValue};
-use std::net::SocketAddr;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-use std::convert::Infallible;
-use dotenv::dotenv; 
-use std::env;
+use std::collections::HashMap;
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let tracer = global::tracer("global_tracer");
-    let _cx = Context::new();
-    let user_agent = extract_user_agent(&req);
+use crate::coordinates::LatLong;
+use crate::tiles::fetch_image_from_point;
+use actix_web::{get, http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
+use actix_web_opentelemetry::RequestTracing;
+use log::{info, warn};
+use tiles::TileSet;
+mod coordinates;
+mod tiles;
 
-    match req.uri().path() {
-        "/" => { 
-            let html_response = generate_index_page();
+mod telemetry_conf;
+use telemetry_conf::init_otel;
 
-            tracer.in_span("operation", |cx| {
-                let span = cx.span();
-                span.set_attribute(Key::new("User Agent").string(user_agent.clone()));
-
-                span.add_event(
-                    format!("Operations"),
-                    vec![
-                        Key::new("Visited Route").string("/"),
-                    ],
-                );
-            });
-
-            Ok(Response::new(Body::from(html_response)))
-        }
-        "/generate_random" => {
-            let random_number = generate_random_number();
-            tracer.in_span("operation", |cx| {
-                let span = cx.span();
-                span.set_attribute(Key::new("User Agent").string(user_agent.clone()));
-                
-                span.add_event(
-                    format!("Operations"),
-                    vec![
-                        Key::new("Visited Route").string("/generate_random"),
-                        Key::new("Random No.").string(random_number.to_string()),
-                    ],
-                );
-                tracer.in_span("Sub operation", |cx| {
-                    let span = cx.span();
-                    span.set_attribute(Key::new("Random No.").string(random_number.to_string()));
-                    span.add_event("Sub span event", vec![]);
-                });
-            });
-            let random_num_response = generate_random_page(random_number);
-            Ok(Response::new(Body::from(random_num_response)))
-        }
-        _ => Ok(Response::builder()
-            .status(404)
-            .body(Body::from("Not Found"))
-            .unwrap()),
-    }
+async fn index() -> impl Responder {
+    "Nothing here"
 }
 
-fn generate_random_number() -> i64 {
-    rand::thread_rng().gen_range(1..100)
+async fn health() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body("{\"status\": \"ok\"}")
 }
 
-// if you want to pass env variables via command
-// fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
-//     opentelemetry_otlp::new_pipeline()
-//         .tracing()
-//         .with_exporter(opentelemetry_otlp::new_exporter().tonic().with_env())
-//         .with_trace_config(
-//             sdktrace::config().with_resource(Resource::default()),
-//         )
-//         .install_batch(opentelemetry::runtime::Tokio)
-// }
+#[get("/images/{long}/{lat}/{size_px}")]
+async fn get_image(
+    path: web::Path<(f64, f64, u32)>,
+    query: web::Query<HashMap<String, String>>,
+) -> impl Responder {
+    let (long, lat, size_px) = path.into_inner();
 
-// if you want to pass env variables via .env file
-fn init_tracer() -> Result<sdktrace::Tracer, TraceError> {
-    let signoz_access_token = std::env::var("SIGNOZ_ACCESS_TOKEN").expect("SIGNOZ_ACCESS_TOKEN not set");
-    let mut metadata = MetadataMap::new();
-    metadata.insert(
-        "signoz-access-token",
-        MetadataValue::from_str(&signoz_access_token).unwrap(),
+    // Extract optional parameters from the query map
+    let radius = query
+        .get("radius")
+        .and_then(|r| r.parse().ok())
+        .unwrap_or(1.0);
+    let tileset = query
+        .get("tileset")
+        .map(|t| match t.as_str() {
+            "swisstopo" => TileSet::Swisstopo,
+            _ => TileSet::Osm,
+        })
+        .unwrap_or(TileSet::Osm);
+
+    info!(
+        latitude = lat,
+        longitude = long;
+        "Fetching image"
     );
-    opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(
-            opentelemetry_otlp::new_exporter()
-                .tonic()
-                .with_metadata(metadata)
-                .with_endpoint(std::env::var("SIGNOZ_ENDPOINT").expect("SIGNOZ_ENDPOINT not set")),
-        )
-        .with_trace_config(
-            sdktrace::config().with_resource(Resource::new(vec![
-                KeyValue::new(
-                    opentelemetry_semantic_conventions::resource::SERVICE_NAME,
-                    std::env::var("APP_NAME").expect("APP_NAME not set"),
-                ),
-            ])),
-        )
-        .install_batch(opentelemetry::runtime::Tokio)
-}
 
-fn extract_user_agent(req: &Request<Body>) -> String {
-    req.headers()
-        .get("user-agent")
-        .and_then(|value| value.to_str().ok())
-        .map_or_else(|| String::from("unknown"), |ua| ua.to_string())
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    dotenv().ok(); //comment this if you want to pass env vars via command
-    let _ = init_tracer()?;
-
-    let port = std::env::var("PORT").expect("PORT not set").parse().expect("PORT must be an Integer");
-    let addr: SocketAddr = ([127, 0, 0, 1], port).into();
-
-    let make_svc = make_service_fn(|_conn| {
-        async { Ok::<_, Infallible>(service_fn(handle_request)) }
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Server running on http://{}", addr);
-
-    if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
+    match fetch_image_from_point(LatLong(lat, long), radius, size_px, tileset).await {
+        Ok(image) => HttpResponse::Ok()
+            .content_type(ContentType::png())
+            .body(image),
+        Err(_) => HttpResponse::InternalServerError().into(),
     }
+}
 
-    shutdown_tracer_provider();
-    Ok(())
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Roll otel errors up to here and log them in aggregate
+    match init_otel() {
+        Ok(_) => {
+            info!("Successfully configured OTel");
+        }
+        Err(err) => {
+            warn!(
+                "Couldn't start OTel! Will proudly soldier on without telemetry: {0}",
+                err
+            );
+        }
+    };
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(RequestTracing::new())
+            .route("/", web::get().to(index))
+            .route("/ping", web::get().to(health))
+            .service(get_image)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
